81780f3ffeca7ae9bcbe993b47c0620bae7caa44
enes-dogan <enesdogan210@gmail.com>
2024-01-08 23:58:58 +0300

    configure notification depending on fetch status

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { CartState } from './types.ts';
 
@@ -7,10 +7,21 @@ import Layout from './components/Layout/Layout.tsx';
 import Products from './components/Shop/Products.tsx';
 
 export default function App() {
+  const [notification, setNotification] = useState({
+    status: '',
+    title: '',
+    message: '',
+  });
   const isOpen = useSelector((state: CartState) => state.cart.isOpen);
   const cartItems = useSelector((state: CartState) => state.cart.cartItems);
 
   useEffect(() => {
+    setNotification({
+      status: 'pending',
+      title: 'Sending...',
+      message: 'Sending cart data!',
+    });
+
     async function sendCartData() {
       const response = await fetch(
         'https://redux-async-b4ba6-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
@@ -19,11 +30,25 @@ export default function App() {
           body: JSON.stringify(cartItems),
         }
       );
+
       if (!response.ok) {
-        throw new Error('Sending cart data failed.');
+        return;
       }
     }
-    void sendCartData();
+
+    setNotification({
+      status: 'success',
+      title: 'Successs!',
+      message: 'Sent cart data successfully.',
+    });
+
+    void sendCartData().catch(() => {
+      setNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart data failed!',
+      });
+    });
   }, [cartItems]);
 
   return (
